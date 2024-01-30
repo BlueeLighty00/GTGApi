26ebbc36602fcb4777e5497098f809cbe2a60513
BlueeLighty00 <davidsernamateu@gmail.com>
2024-01-30 16:01:38 +0100

    First push

---
+++ src/test/java/org/example/gtgapi/GtgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gtgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtgApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
