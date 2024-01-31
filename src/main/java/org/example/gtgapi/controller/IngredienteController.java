848029b67fa0c8d0f7bddfdde235f5ccaecc4370
alurue9140 <alurue9140@ieselcaminas.org>
2024-01-31 19:38:19 +0100

    CONTROLLERS AND TEMPLATES ALMOST FINISHED

--- src/main/java/org/example/gtgapi/controller/IngredienteController.java
+++ src/main/java/org/example/gtgapi/controller/IngredienteController.java
@@ -2,7 +2,6 @@ package org.example.gtgapi.controller;
 
 import org.example.gtgapi.models.dao.IngredienteDAOImpl;
 import org.example.gtgapi.models.entity.Ingrediente;
-import org.example.gtgapi.models.entity.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,53 +10,53 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class IngredienteController {
     @Autowired
-    IngredienteDAOImpl IngredienteDao;
+    IngredienteDAOImpl ingredienteDao;
 
     @GetMapping("/ingredientes")
-    public String menus(Model model) {
-        model.addAttribute("ingredientes", IngredienteDao.findAll());
+    public String ingredientes(Model model) {
+        model.addAttribute("ingredientes", ingredienteDao.findAll());
         return "ingredientes";
     }
 
     @GetMapping("/ingrediente/{id}")
-    public String getMenuDetails(Model model, @PathVariable Long id) {
-        Ingrediente ingrediente = IngredienteDao.findById(id);
+    public String getIngredienteDetails(Model model, @PathVariable Long id) {
+        Ingrediente ingrediente = ingredienteDao.findById(id);
         model.addAttribute("ingrediente", ingrediente);
         return "details_ingrediente";
     }
 
     @GetMapping("/ingrediente/edit/{id}")
-    public String getMenuEdit(Model model, @PathVariable Long id) {
-        Ingrediente ingrediente  = IngredienteDao.findById(id);
+    public String getIngredienteEdit(Model model, @PathVariable Long id) {
+        Ingrediente ingrediente  = ingredienteDao.findById(id);
         model.addAttribute("ingrediente", ingrediente);
         return "edit_ingrediente";
     }
 
     @GetMapping("/ingrediente/create")
-    public String createMenu(Model model) {
+    public String createIngrediente(Model model) {
         model.addAttribute("ingrediente", new Ingrediente());
         return "create_ingrediente";
     }
 
     @PostMapping("/ingrediente/create")
-    public String newMenu(@ModelAttribute("ingrediente") Ingrediente ingrediente) {
+    public String newIngrediente(@ModelAttribute("ingrediente") Ingrediente ingrediente) {
 
-        IngredienteDao.save(ingrediente);
+        ingredienteDao.save(ingrediente);
         return "redirect:/ingredientes";
     }
 
 
     @PostMapping("/ingrediente/update/{id}")
-    public String updateMenu(@ModelAttribute("menu") Ingrediente ingrediente) {
+    public String updateIngrediente(@ModelAttribute("ingrediente") Ingrediente ingrediente) {
 
-        IngredienteDao.update(ingrediente);
+        ingredienteDao.update(ingrediente);
         return "redirect:/ingredientes";
     }
 
     @DeleteMapping("/ingrediente/delete/{id}")
-    public String deleteMenu(@PathVariable Long id) {
+    public String deleteIngrediente(@PathVariable Long id) {
 
-        IngredienteDao.delete(id);
+        ingredienteDao.delete(id);
         return "redirect:/ingredientes";
     }
 }
--- src/main/java/org/example/gtgapi/controller/MenuController.java
+++ src/main/java/org/example/gtgapi/controller/MenuController.java
@@ -1,36 +1,34 @@
 package org.example.gtgapi.controller;
 
 
-import jakarta.persistence.EntityNotFoundException;
 import org.example.gtgapi.models.dao.MenuDAOImpl;
 import org.example.gtgapi.models.entity.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MenuController {
     @Autowired
-    MenuDAOImpl MenuService;
+    MenuDAOImpl menuDAO;
 
     @GetMapping("/menus")
     public String menus(Model model) {
-        model.addAttribute("menus", MenuService.findAll());
+        model.addAttribute("menus", menuDAO.findAll());
         return "menus";
     }
 
     @GetMapping("/menu/{id}")
     public String getMenuDetails(Model model, @PathVariable Long id) {
-        Menu menu = MenuService.findById(id);
+        Menu menu = menuDAO.findById(id);
         model.addAttribute("menu", menu);
         return "details_menu";
     }
 
     @GetMapping("/menu/edit/{id}")
     public String getMenuEdit(Model model, @PathVariable Long id) {
-        Menu menu = MenuService.findById(id);
+        Menu menu = menuDAO.findById(id);
         model.addAttribute("menu", menu);
         return "edit_menu";
     }
@@ -44,7 +42,7 @@ public class MenuController {
     @PostMapping("/menu/create")
     public String newMenu(@ModelAttribute("menu") Menu menu) {
 
-        MenuService.save(menu);
+        menuDAO.save(menu);
         return "redirect:/menus";
     }
 
@@ -52,15 +50,15 @@ public class MenuController {
     @PostMapping("/menu/update/{id}")
     public String updateMenu(@ModelAttribute("menu") Menu menu) {
 
-        MenuService.update(menu);
+        menuDAO.update(menu);
         return "redirect:/menus";
     }
 
     @DeleteMapping("/menu/delete/{id}")
     public String deleteMenu(@PathVariable Long id) {
-        MenuService.delete(id);
+        menuDAO.delete(id);
         Menu menuLibanes = new Menu();
-        MenuService.save(menuLibanes);
+        menuDAO.save(menuLibanes);
         return "redirect:/menus";
     }
 }
